90fa855ce2047bced66b7946346a21bcfbe3430f
Ivan Homziak <ivan.homziak@ing.com>
2026-03-04 22:56:17 +0100

    ref

--- src/main/java/com/example/securitymicroservice/SecurityMicroserviceExampleApplication.java
+++ src/main/java/com/example/securitymicroservice/SecurityMicroserviceExampleApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-/** Application bootstrap class. */
 public class SecurityMicroserviceExampleApplication {
 
     public static void main(String[] args) {
